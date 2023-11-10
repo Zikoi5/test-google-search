25138f9b9a1a41cc3a2bb8a17365adba4a937091
Ziloliddin <gandgziko@gmail.com>
2023-11-10 09:41:59 +0500

    Input change handler was add

--- src/components/pages/login.vue
+++ src/components/pages/login.vue
@@ -3,11 +3,13 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const MAX_LENGTH = 16;
+const TIMEOUT = 3;
 const router = useRouter();
 const form = ref({
   username: "",
   token: ""
 });
+const pending = ref(false);
 
 const onChange = (e) => {
   const val = e.target.value;
@@ -21,9 +23,13 @@ const onChange = (e) => {
 };
 
 const onSubmit = () => {
-  router.push({
-    name: "Home"
-  });
+  pending.value = true;
+  setTimeout(() => {
+    router.replace({
+      name: "Home"
+    });
+    pending.value = false;
+  }, TIMEOUT * 1000);
 };
 </script>
 
@@ -57,16 +63,21 @@ const onSubmit = () => {
           id="token"
           v-model="form.token"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="•••••••••"
+          placeholder="Token"
           required
+          @input="onChange"
         />
       </div>
 
       <button
+        :disabled="pending"
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        :class="[
+          { 'disabled:opacity-50 disabled:pointer-events-none': pending }
+        ]"
       >
-        Submit
+        {{ pending ? "Pending" : "Submit" }}
       </button>
     </form>
   </div>
